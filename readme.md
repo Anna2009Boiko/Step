264d26db62d8d5f8ee357fec6385ef4e32743a51
Anna <anna2009boiko@gmail.com>
2023-02-28 21:14:26 +0200

    first commit

---
+++ readme.md
@@ -0,0 +1,5 @@
+Список використаних технологий: HTML, SASS, JavaScript, NodeJS, Gulp, BEM
+Склад учасників: Anna Boiko, Oleksandra Topka
+Завдання виконувала Anna Boiko: верстання шапки сайту з верхнім меню (включаючи випадаюче меню при малій роздільній здатності екрана), верстання секції People Are Talking About Fork
+Завдання виконувала Oleksandra Topka: верстання  блоку Revolutionary Editor, верстання секції Here is what you get та Fork Subscription 
+настройка Gulp для проекту - спільна збірка
---
+++ src/js/scripts.js
@@ -0,0 +1,48 @@
+import * as flsFunctions from "./modules/functions.js";
+
+flsFunctions.isWebp();
+
+/*
+import Swiper, { Navigation, Pagination } from 'swiper';
+const swiper = new Swiper();
+*/
+
+'use strict';
+document.addEventListener(`DOMContentLoaded`, () => {
+	//= ./../sections/header/header.js
+	//= ./../sections/features/features.js
+});
+
+const body = document.body;
+const menu = document.querySelector('.menu');
+const menuTogleBtn = document.querySelector('.menu-btn');
+const menuTogleBtnLines = [...document.querySelectorAll('.menu-btn__line')];
+
+body.addEventListener('click', e => {
+	if (body.clientWidth <= 480) {
+		const target = e.target;
+		if (target === menuTogleBtn || target.classList.contains('menu-btn__line')) {
+			body.classList.toggle('menu-is-open');
+		} else {
+			body.classList.remove('menu-is-open');
+		}
+		toggleMenu();
+
+	}
+});
+
+function toggleMenu() {
+	if (body.classList.contains('menu-is-open')) {
+		menu.style.opacity = 1;
+		menu.style.visibility = 'visible';
+		menuTogleBtnLines.forEach(line => {
+			line.classList.add('menu-btn__line--open');
+		});
+	} else {
+		menu.style.opacity = 0;
+		menu.style.visibility = 'hidden';
+		menuTogleBtnLines.forEach(line => {
+			line.classList.remove('menu-btn__line--open');
+		});
+	}
+}
